9b179b0860359a421324aba1e72d7e87e789e270
Reaper8202 <77172561+Reaper8202@users.noreply.github.com>
2023-05-10 23:13:26 -0400

    Update subtitle overlay.py

--- subtitle overlay.py
+++ subtitle overlay.py
@@ -1,12 +1,14 @@
 from moviepy.editor import TextClip, CompositeVideoClip, VideoFileClip
 from moviepy.video.tools.subtitles import SubtitlesClip
 
+main_path = "/Users/derek/Desktop/Python Code/Reddit Optimization/output.mp4"
+subtitle_path = "/Users/derek/Desktop/Python Code/Reddit Optimization/Reddit_Bot_2/subtitles.srt"
+final_path = "/Users/derek/Desktop/Python Code/output_subtitled.mp4"
 
-
-main_video = VideoFileClip("C:\\Users\\billa\\Desktop\\PYTHON STUFF\\youtube\\M1.mp4")
+main_video = VideoFileClip(main_path)
 generator = lambda txt: TextClip(txt, font="arial", fontsize=50, color="white", method='caption', size=main_video.size)
 
-sub_clip = SubtitlesClip('C:\\Users\\billa\\Desktop\\PYTHON STUFF\\youtube\\subtitles.srt', generator)
+sub_clip = SubtitlesClip(subtitle_path, generator)
 
-result = CompositeVideoClip((main_video, sub_clip), size=main_video.size)
-result.write_videofile('C:\\Users\\billa\\Desktop\\PYTHON STUFF\\youtube\\output.mp4', fps=main_video.fps, temp_audiofile="temp-audio.m4a", remove_temp=True, codec="libx264", audio_codec="aac")
+result = CompositeVideoClip([main_video, sub_clip.set_position(('center', 'bottom'))], size=main_video.size)
+result.write_videofile(final_path, fps=main_video.fps, temp_audiofile="temp-audio.m4a", remove_temp=True, codec="libx264", audio_codec="aac")
