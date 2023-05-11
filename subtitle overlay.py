d13d64c1a3928370a418b1b2540418ed18779bac
JosephLiao542211 <100002111+JosephLiao542211@users.noreply.github.com>
2023-05-10 22:10:03 -0400

    Update subtitle overlay.py

--- subtitle overlay.py
+++ subtitle overlay.py
@@ -1,51 +1,12 @@
-import sys
-import pysrt
-from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
-
-# Place files in this path or modify the paths to point to where the files are
-srtfilename = "/content/gdrive/My Drive/Burn_Subtitles_Into_Video/AI_SaaS.srt"
-mp4filename = "/content/gdrive/My Drive/Burn_Subtitles_Into_Video/AI_SaaS.mp4"
-
-def time_to_seconds(time_obj):
-    return time_obj.hours * 3600 + time_obj.minutes * 60 + time_obj.seconds + time_obj.milliseconds / 1000
+from moviepy.editor import TextClip, CompositeVideoClip, VideoFileClip
+from moviepy.video.tools.subtitles import SubtitlesClip
 
 
-def create_subtitle_clips(subtitles, videosize, fontsize=24, font='Arial', color='yellow', debug=False):
-    subtitle_clips = []
 
-    for subtitle in subtitles:
-        start_time = time_to_seconds(subtitle.start)
-        end_time = time_to_seconds(subtitle.end)
-        duration = end_time - start_time
+main_video = VideoFileClip("C:\\Users\\billa\\Desktop\\PYTHON STUFF\\youtube\\M1.mp4")
+generator = lambda txt: TextClip(txt, font="arial", fontsize=50, color="white", method='caption', size=main_video.size)
 
-        video_width, video_height = videosize
+sub_clip = SubtitlesClip('C:\\Users\\billa\\Desktop\\PYTHON STUFF\\youtube\\subtitles.srt', generator)
 
-        text_clip = TextClip(subtitle.text, fontsize=fontsize, font=font, color=color, bg_color='black',
-                             size=(video_width * 3 / 4, None), method='caption').set_start(start_time).set_duration(
-            duration)
-        subtitle_x_position = 'center'
-        subtitle_y_position = video_height * 4 / 5
-
-        text_position = (subtitle_x_position, subtitle_y_position)
-        subtitle_clips.append(text_clip.set_position(text_position))
-
-    return subtitle_clips
-
-
-# Load video and SRT file
-video = VideoFileClip(mp4filename)
-subtitles = pysrt.open(srtfilename)
-
-begin, end = mp4filename.split(".mp4")
-output_video_file = begin + '_subtitled' + ".mp4"
-
-print("Output file name: ", output_video_file)
-
-# Create subtitle clips
-subtitle_clips = create_subtitle_clips(subtitles, video.size)
-
-# Add subtitles to the video
-final_video = CompositeVideoClip([video] + subtitle_clips)
-
-# Write output video file
-final_video.write_videofile(output_video_file)
+result = CompositeVideoClip((main_video, sub_clip), size=main_video.size)
+result.write_videofile('C:\\Users\\billa\\Desktop\\PYTHON STUFF\\youtube\\output.mp4', fps=main_video.fps, temp_audiofile="temp-audio.m4a", remove_temp=True, codec="libx264", audio_codec="aac")
